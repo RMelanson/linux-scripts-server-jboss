b84f3c19895d6c47bb590ad7d7df1a9eeac4a34e
Robin Melanson <remcogold@gmail.com>
2018-05-23 22:56:45 -0400

    Delete devToolsBootstrap.sh

--- devToolsBootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-currDir=$PWD
-devToolsDir=/tmp/scripts/devtools
-git clone https://github.com/RMelanson/linux-scripts-devtools.git $devToolsDir
-cd $devToolsDir
-chmod 744 setup.sh
-./setup.sh
-cd $currDir
