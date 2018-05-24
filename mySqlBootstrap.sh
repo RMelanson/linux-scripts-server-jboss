75ce273e3b0fbabf53222d7a1dd0b560e847f500
Robin Melanson <remcogold@gmail.com>
2018-05-23 22:56:59 -0400

    Delete mySqlBootstrap.sh

--- mySqlBootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-currDir=$PWD
-mySqlDir=/tmp/scripts/apps/db/MYSQL
-git clone https://github.com/RMelanson/linux-scripts-apps-db-mysql.git $mySqlDir
-cd $mySqlDir
-./setup.sh
-cd $currDir:
