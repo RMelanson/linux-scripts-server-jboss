6561102d304a2f165349f5f556f178b6e44b71cc
Robin Melanson <remcogold@gmail.com>
2018-05-25 14:26:33 -0400

    Update and rename configurejBoss.sh to configurejBossRemoting.sh

--- install/configurejBossRemoting.sh
+++ install/configurejBossRemoting.sh
@@ -1,6 +1,12 @@
 #ToDo Configure JBOSS
 wfHome=$1
+admUser=$2
 
-# Backup original standalone.xml and configure the local hostname for remoting
+# ADD WILDFLY REMOTE ADMIN USER
+$wfHome/bin/add-user.sh $admUser admin --silent
+
+# BACKUP ORIGINAL standalone.xml
 cp $wfHome/standalone/configuration/standalone.xml $wfHome/standalone/configuration/standalone.xml.BAK
+
+# CONFIGURE THE STANDALONE LOCAL HOSTNAME FOR REMOTE ADMIN
 sed -i "s/127.0.0.1/$(hostname -I)/g" $wfHome/standalone/configuration/standalone.xml
