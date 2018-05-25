cda0b182bfe50717f578fe21ef7a249b32d0204c
Robin Melanson <remcogold@gmail.com>
2018-05-25 14:13:01 -0400

    Update configurejBoss.sh

--- install/configurejBoss.sh
+++ install/configurejBoss.sh
@@ -1,2 +1,6 @@
 #ToDo Configure JBOSS
 wfHome=$1
+
+# Backup original standalone.xml and configure the local hostname for remoting
+cp $wfHome/standalone/configuration/standalone.xml $wfHome/standalone/configuration/standalone.xml.BAK
+sed -i "s/127.0.0.1/$(hostname -I)/g" $wfHome/standalone/configuration/standalone.xml
