4b87ad5a900318408b0a13365c089cd810668707
Robin Melanson <remcogold@gmail.com>
2018-05-25 16:04:53 -0400

    Update installjBoss10.sh

--- install/installjBoss10.sh
+++ install/installjBoss10.sh
@@ -1,7 +1,9 @@
 # WILDFLY INITIAL INSTALLATION SETUP
 wfHome=$1
+wfOwner$2
+wfGroup=$3
+wfLog=$4
 wfPkg=wildfly-10.0.0.Final.tar.gz
-wfLog=/var/log/wildfly
 
 # DOWNLOAD WILDFLY
 wget http://download.jboss.org/wildfly/10.0.0.Final/wildfly-10.0.0.Final.tar.gz
@@ -12,7 +14,7 @@ tar -xzf $wfPkg -C $wfHome --strip 1
 echo y | rm $wfPkg 
 
 # CHANGE TO APPROPRIATE OWNER:GROUP
-chown -R wildfly:wildfly $wfHome
+chown -R $wfOwner:$wfGroup $wfHome
 
 mkdir -p $wfLog 
-chown -R wildfly:wildfly $wfLog
+chown -R $wfOwner:$wfGroup $wfLog
