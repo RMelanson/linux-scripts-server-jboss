a4ca578e9996eb30ca734053f905af241647948c
Robin Melanson <remcogold@gmail.com>
2018-05-25 14:06:55 -0400

    Update installjBoss.sh

--- install/installjBoss.sh
+++ install/installjBoss.sh
@@ -1,1 +1,21 @@
-# Todo DownLoad and Install JBOSS
+wfPkg=wildfly-10.0.0.Final.tar.gz
+wfPkg=$1
+wfHome=$2
+
+wget http://download.jboss.org/wildfly/10.0.0.Final/wildfly-10.0.0.Final.tar.gz
+echo EXECUTING tar -xzf $wfPkg -C $wfHome --strip 1
+tar -xzf $wfPkg -C $wfHome --strip 1
+echo y | rm $wfPkg 
+cd  $wfHome
+
+#finally chown and groups to wildfly for home directory objects
+
+chown -R wildfly:wildfly ~wildfly
+
+wfLog=/var/log/wildfly
+mkdir -p $wfLog 
+chown -R wildfly:wildfly $wfLog
+# install wildfly as service
+
+# add wildfly Admin User
+$wfHome/bin/add-user.sh admin admin --silent
