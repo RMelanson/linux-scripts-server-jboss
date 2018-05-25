e35f0c12d091424e7adcf4502bad9bed74bacfaa
root <root@ip-172-31-13-61.ca-central-1.compute.internal>
2018-05-25 19:42:47 +0000

    WIP

--- install/exitIfInstalled.sh
+++
@@ -1,8 +0,0 @@
-# EXIT IF INSTALLED
-# VALIDATE THE INSTALLATION HOME DIRECTORY AND IF EXISTS, EXIT SCRIPT
-
-wfHome=$1
-if [ -d $wfHome ]; then
-    echo WildFly Already installed EXITING
-    return
-fi
