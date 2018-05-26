d992346faf7809042d2731c7850bf930e2fa7179
Robin Melanson <remcogold@gmail.com>
2018-05-25 21:52:41 -0400

    Update jbossBootstrap.sh

--- jbossBootstrap.sh
+++ jbossBootstrap.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 jbossBootCurrDir=$PWD
 
-if [ -z "$1" ]; then
-   branch=master
-else
-   branch=$1
-fi
-
 # Ensure script is running under root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root or under sudo"
   exit -1
+fi
+
+if [ -z "$1" ]; then
+   branch=master
+else
+   branch=$1
 fi
 
 #Set Cloning Properties
