b948a633cc20c441730218c3903bc3c8d577ed60
Robin Melanson <remcogold@gmail.com>
2018-05-25 21:58:38 -0400

    Update jbossBootstrap.sh

--- jbossBootstrap.sh
+++ jbossBootstrap.sh
@@ -18,9 +18,9 @@ pkg=JBOSS
 gitRepo="linux-scripts-apps-jboss.git $jbossDir"
 installDir="/tmp/scripts/apps/JBOSS"
 if [ -f ~/.ssh/gitHub.key ]; then
-   clone="git clone git@github.com:RMelanson/"
+   clone="git clone -b $branch git@github.com:RMelanson/"
 else
-   clone="git clone https://github.com/RMelanson/"
+   clone="git clone -b $branch https://github.com/RMelanson/"
 fi
 
 # Clone $pkg
