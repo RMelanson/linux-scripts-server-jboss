ca5256d698f40191b1944d4e079c976c34d93618
Robin Melanson <remcogold@gmail.com>
2018-05-25 20:13:12 -0400

    Update jbossBootstrap.sh

--- jbossBootstrap.sh
+++ jbossBootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 jbossBootCurrDir=$PWD
+branch=dev
 # Ensure script is running under root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root or under sudo"
@@ -11,9 +12,9 @@ pkg=JBOSS
 gitRepo="linux-scripts-apps-jboss.git $jbossDir"
 installDir="/tmp/scripts/apps/JBOSS"
 if [ -f ~/.ssh/gitHub.key ]; then
-   clone="git clone git@github.com:RMelanson/"
+   clone="git clone -b $branch git@github.com:RMelanson/"
 else
-   clone="git clone https://github.com/RMelanson/"
+   clone="git clone -b $branch https://github.com/RMelanson/"
 fi
 
 # Clone $pkg
