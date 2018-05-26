9160fd380b6f817421ba387d434d93e539b27ba7
Robin Melanson <remcogold@gmail.com>
2018-05-26 17:18:13 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -36,31 +36,6 @@ chown -R wildfly:wildfly $wfLog
 # add wildfly Admin User
 $wfHome/bin/add-user.sh admin admin --silent
 
-#------------------- SET UP WILDFLY CONFIGURATION ---------------
-# ./install/configurejBoss.sh
-#Copy init scripts
-
-#echo y | cp $wfHome/docs/contrib/scripts/init.d/wildfly-init-redhat.sh /etc/init.d/wildfly
-
-#echo y | rm -f /etc/default/wildfly.conf
-#cp $wfHome/docs/contrib/scripts/init.d/wildfly.conf /etc/default
-#sed -i 's/# JAVA_HOME="\/usr\/lib\/jvm\/default-java"/JAVA_HOME=\/usr\/lib\/jvm\/java-1.8.0/g' /etc/default/wildfly.conf
-#sed -i 's/# JBOSS_HOME/JBOSS_HOME/g' /etc/default/wildfly.conf
-#sed -i 's/# JBOSS_USER/JBOSS_USER/g' /etc/default/wildfly.conf
-#sed -i 's/# JBOSS_CONFIG/JBOSS_CONFIG/g' /etc/default/wildfly.conf
-#sed -i 's/# STARTUP_WAIT/STARTUP_WAIT/g' /etc/default/wildfly.conf
-#sed -i 's/# SHUTDOWN_WAIT/SHUTDOWN_WAIT/g' /etc/default/wildfly.conf
-#sed -i 's/# JBOSS_CONSOLE_LOG/JBOSS_CONSOLE_LOG/g' /etc/default/wildfly.conf
-
-#Set the local hostname
-#cp $wfHome/standalone/configuration/standalone.xml $wfHome/standalone/configuration/standalone.xml.BAK
-#sed -i "s/127.0.0.1/$(hostname -I)/g" $wfHome/standalone/configuration/standalone.xml
-
-#------------------- ADD WILDFLY AS A SERVICE AND START WILDFLY SERVICE ---------------
-
-#chkconfig --add wildfly
-#chkconfig wildfly on
-#service wildfly start
 ##################################################################################
 # CONFIGURE WILDFLY AS A SERVICE AND START WILDFLY SERVICE
 ./install/addjBossAsService.sh $wfHome 
