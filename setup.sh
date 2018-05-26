10f06faea7ad40016e8a837976a2c74f443faa0a
Robin Melanson <remcogold@gmail.com>
2018-05-26 11:50:21 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,12 +11,12 @@ then
     return
 fi
 
-wfPkg=wildfly-10.0.0.Final.tar.gz
-wfHome=/opt/wildfly
-wfAdmin=wildfly
-wfGroup=wildfly
-wfJava=java-1.8.0-openjdk-devel
-pkg=wildfly
+#wfPkg=wildfly-10.0.0.Final.tar.gz
+#wfHome=/opt/wildfly
+#wfAdmin=wildfly
+#wfGroup=wildfly
+#wfJava=java-1.8.0-openjdk-devel
+#pkg=wildfly
 
 # DOWNLOAD AND INSTALL JAVA 8 AND MAKE DEFAULT
 ./install/installJava8.sh
