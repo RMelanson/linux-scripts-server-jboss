cfceab0e7a39ce63db97e3e1d6029b33e1ff8ab4
Robin Melanson <remcogold@gmail.com>
2018-05-26 18:50:41 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,24 +13,6 @@
 # DOWNLOAD AND INSTALL WILDFLY 10
 ./install/installjBoss10.sh $wfOwner $wfGroup $wfHome $wfLog
 
-#########################################################################
-
-#wget http://download.jboss.org/wildfly/10.0.0.Final/wildfly-10.0.0.Final.tar.gz
-#echo EXECUTING tar -xzf $wfPkg -C $wfHome --strip 1
-#tar -xzf $wfPkg -C $wfHome --strip 1
-#echo y | rm $wfPkg 
-
-#finally chown and groups to wildfly for home directory objects
-
-#mkdir -p $wfLog
-#chown -R wildfly:wildfly $wfHome
-#chown -R wildfly:wildfly $wfLog
-# install wildfly as service
-
-# add wildfly Admin User
-#$wfHome/bin/add-user.sh admin admin --silent
-
-##################################################################################
 # SET UP WILDFLY REMOTING CONFIGURATION 
 ./install/configjBossRemoting.sh $wfHome $wfAdmin
 
