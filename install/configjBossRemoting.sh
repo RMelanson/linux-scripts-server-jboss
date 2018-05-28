4a641fae53f4914f9d0443f670af97b4aa2fcaca
root <root@ip-172-31-13-254.ca-central-1.compute.internal>
2018-05-28 22:40:55 +0000

    WIP

--- install/configjBossRemoting.sh
+++ install/configjBossRemoting.sh
@@ -9,4 +9,4 @@ $wfHome/bin/add-user.sh $admUser admin --silent
 cp $wfHome/standalone/configuration/standalone.xml $wfHome/standalone/configuration/standalone.xml.BAK
 
 # CONFIGURE THE STANDALONE LOCAL HOSTNAME FOR REMOTE ADMIN
-sed -i "s/127.0.0.1/$(hostname -I)/g" $wfHome/standalone/configuration/standalone.xml
+sed -i "s/127.0.0.1/$(hostname -I|sed 's/ //g)/g" $wfHome/standalone/configuration/standalone.xml
