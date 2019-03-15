b2ecf0117b1156fdd698ead0b07eb1a217da7604
Robin Melanson <remcogold@gmail.com>
2019-03-14 20:53:59 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # JBOSS WILDFLY CONFIGURATION PARAMETERS
+gitRepo="linux-scripts-apps-jboss.git $jbossDir"
+installDir="/tmp/scripts/apps/JBOSS"
 pkg=wildfly
 wfAdmin=admin
 wfOwner=wildfly
