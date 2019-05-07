f6bf0d71cd3e56e0d27345e6a2b0aa411ea09810
Robin Melanson <remcogold@gmail.com>
2019-05-07 14:21:28 -0400

    Update jbossBootstrap.sh

--- jbossBootstrap.sh
+++ jbossBootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 bootstrapDir=$PWD
-bootstrap=$bootstrapDir/devToolsBootstrap.sh
+bootstrap=$bootstrapDir/ZZZZZZZZZZZZZZZZ.sh
 
 # Ensure script is running under root
 if [ "$EUID" -ne 0 ]
