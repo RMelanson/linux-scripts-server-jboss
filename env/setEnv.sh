0ec9e8a7f3b6fa6f7866716d9aaaf61f8643046c
Robin Melanson <robin.e.melanson@gmail.com>
2019-07-31 00:27:55 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -15,3 +15,12 @@ wfOwner=wildfly
 wfGroup=wildfly
 wfHome=/opt/wildfly
 wfLog=/var/log/wildfly
+
+pkgOwner=ec2-user
+
+echo Setting External Args
+echo These Arguments Overwrite Default Argument Settings
+for arg in "$@"; do
+  echo setArgs EXECUTING: export $arg
+  export $arg
+done
